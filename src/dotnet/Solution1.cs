673943d0a3930640d30654e1b41a67e9b6d17de4
Mike Yachanin <michael@yachanin.com>
2020-06-27 17:37:28 -0700

    Solve problem 1

---
+++ src/dotnet/Solution1.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace dotnet
+{
+    /* If we list all the natural numbers below 10 that are multiples of 3 or 5,
+       we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+       Find the sum of all the multiples of 3 or 5 below 1000.
+    */
+    public class Solution1
+    {
+        static void Main()
+        {
+            Console.WriteLine(new Solution1().Solve());
+        }
+
+        public string Solve()
+        {
+            int sum = 0;
+
+            for (int i=0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum.ToString();
+        }
+    }
+}
